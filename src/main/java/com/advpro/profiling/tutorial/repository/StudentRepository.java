1f6e8bee3b1ca9d269c89e2b6c6e8b474e573823
alphasuthamedia <alphasuthamedia@gmail.com>
2025-03-14 15:38:37 +0700

    [Refactoring] - Optimizing get highest gpa endpoint

--- src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
+++ src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
@@ -4,9 +4,15 @@ import com.advpro.profiling.tutorial.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
+
 /**
  * @author muhammad.khadafi
  */
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    @Query("SELECT s FROM Student s ORDER BY s.gpa DESC LIMIT 1")
+    Optional<Student> findStudentWithHighestGpa();
 }
